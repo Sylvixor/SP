a82009a79c69f5e8bd523400ecea644ebeccc79e
thelegendaryjohn <legendaryjohn2005@gmail.com>
2024-11-20 14:24:14 +0700

    Testing Open Graph again

--- src/pages/og/[...path].ts
+++ src/pages/og/[...path].ts
@@ -6,27 +6,34 @@ const entries = await getCollection("docs");
 
 // Map the entry array to an object with the page ID as key and the
 // frontmatter data as value.
-const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]));
-
+const pages = Object.fromEntries(entries.map(({ slug, data }) => [slug, data]));
 export const { getStaticPaths, GET } = OGImageRoute({
 	// Pass down the documentation pages.
 	pages,
 	// Define the name of the parameter used in the endpoint path, here `paths`
 	// as the file is named `[...paths].ts`.
-	param: "paths",
+	param: "path",
 	// Define a function called for each page to customize the generated image.
-	getImageOptions: (_path, page: (typeof pages)[number]) => {
-		return {
-			// Use the page title and description as the image title and description.
-			title: page.data.title,
-			description: page.data.description,
-			// bgImage: {
-			// 	path: "index.png",
-			// },
-			// Customize various colors and add a border.
-			bgGradient: [[24, 24, 27]],
-			border: { color: [63, 63, 70], width: 20 },
-			padding: 120,
-		};
-	},
+	getImageOptions: (path, page) => ({
+		title: page.title,
+		description: page.description,
+		// logo: {
+		// 	// This is actually transparent
+		// 	// The logo is embedded in the BG image to control positioning
+		// 	path: "./src/assets/og-logo.png",
+		// },
+		bgImage: {
+			path: "./src/assets/og-bg.png",
+			fit: "none",
+		},
+		font: {
+			title: {
+				weight: "Black",
+			},
+			description: {
+				weight: "Medium",
+				size: 32,
+			},
+		},
+	}),
 });
