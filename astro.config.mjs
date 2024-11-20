08159033fa05278a76673355f7ad71a6271288b8
thelegendaryjohn <legendaryjohn2005@gmail.com>
2024-11-20 10:06:50 +0700

    More testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
 			customCss: ["./src/styles/custom.css"],
 			credits: true,
 			favicon: "/favicon.png",
+			components: {
+				Head: "./src/components/Head.astro",
+			},
 			logo: {
 				src: "./src/assets/SP-logo-nav.png",
 			},
@@ -26,7 +29,7 @@ export default defineConfig({
 					label: "Introduction",
 					items: [
 						{ label: "FAQ", link: "/introduction/faq" },
-						{ label: "Common issues", link: "/introduction/issues"},
+						{ label: "Common issues", link: "/introduction/issues" },
 						{ label: "Homebrewable?", link: "/introduction/homebrewable" },
 						{ label: "Avoid bans", link: "/introduction/avoid-ban" },
 					],
@@ -35,10 +38,13 @@ export default defineConfig({
 					label: "Homebrewing",
 					items: [
 						{ label: "Unpatched", link: "/homebrewing/unpatched" },
-						{ label: "Possibly Patched", link: "/homebrewing/possibly-patched" },
+						{
+							label: "Possibly Patched",
+							link: "/homebrewing/possibly-patched",
+						},
 						{ label: "Patched", link: "/homebrewing/patched" },
 						{ label: "Mariko", link: "/homebrewing/mariko" },
-					]
+					],
 				},
 				{
 					label: "Important",
@@ -53,27 +59,33 @@ export default defineConfig({
 						{ label: "Dump titles", link: "/important/titles" },
 						{ label: "Dump keys", link: "/important/keys" },
 						{ label: "Dump firmware", link: "/important/firmware" },
-					]
+					],
 				},
 				{
 					label: "Miscellaneous",
 					items: [
-						{ label: "Partition-based or file-based", link: "/miscellaneous/partition" },
+						{
+							label: "Partition-based or file-based",
+							link: "/miscellaneous/partition",
+						},
 						{ label: "Install homebrew apps", link: "/miscellaneous/homebrew" },
 						{ label: "Install titles via USB", link: "/miscellaneous/usb" },
 						{ label: "Install themes", link: "/miscellaneous/themes" },
-						{ label: "Enable gamecard reader", link: "/miscellaneous/gamecard" },
+						{
+							label: "Enable gamecard reader",
+							link: "/miscellaneous/gamecard",
+						},
 						{ label: "Manage cheats", link: "miscellaneous/cheats" },
 						{ label: "Manage sysmodules", link: "/miscellaneous/sysmodules" },
 						{ label: "Setup Tesla overlay", link: "/miscellaneous/tesla" },
-						{ label: "Setup Incognito", link: "/miscellaneous/incognito"},
+						{ label: "Setup Incognito", link: "/miscellaneous/incognito" },
 						{ label: "Verify dumps", link: "/miscellaneous/verify" },
 						{ label: "Emulating Amiibo", link: "/miscellaneous/amiibo" },
 						{ label: "MicroSD migration", link: "/miscellaneous/migrate" },
 						{ label: "SXOS migration", link: "/miscellaneous/sxos" },
 						{ label: "About", link: "/miscellaneous/about" },
-					]
-				}
+					],
+				},
 			],
 		}),
 	],
